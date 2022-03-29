8eaf7c496ded903dc2954ae111963e932c60ac41
Khhs167 <khhs1671@gmail.com>
2022-03-29 19:40:20 +0200

    Stack tests!

--- boot_sect.asm
+++ boot_sect.asm
@@ -1,34 +1,34 @@
 ;
-;  Basic find-the-byte
+;  Basic stack stuff
 ;
 
-[org 0x7c00] ; Affset all memory readings to where BIOS would start loading the bootloader
+;[org 0x7c00] ; Affset all memory readings to where BIOS would start loading the bootloader
 
 mov ah, 0x0e ; int 10/ah = 0eh -> print
 
+mov bp, 0x8000 ; Set base of stack above boot sector
+mov sp, bp ; Resets stack
 
-; First attempt
-mov al, the_secret
-int 0x10
+; Push these chars onto stack. They will be convertex to 16-bit by appending 0x00 to the end.
+; The value wont change but the size doubles(0x01 becomes 0x0100 wich both is worth 1)
 
-; Second attempt
-mov al, [the_secret]
-int 0x10
+push 'A'
+push 'B'
+push 'C'
 
-; Third attempt
-mov bx, the_secret
-add bx, 0x7c00
-mov al, [bx]
-int 0x10
+pop bx ; Pop the current 16-bit stack value. Note that this is 16 bits(one word)
+mov al, bl ; Copy the low bits of bl to al(0x12 in the number 0x1234)
+int 0x10 ; Print al
 
-; Attempt 4
-mov al, [0x7c1e]
-int 0x10
+pop bx ; Pop next value
+mov al, bl
+int 0x10 ; Print al
 
-jmp $ ; Jump to current(while true be like)
+mov al, [0x7ffe] ; To prove the stack grows down, fetch the char at
+                ; 0x8000 - 0x2 (i.e 16 bits)
+int 0x10        ; print(al)
 
-the_secret:
-    db "X"
+jmp $ ; Just hang the CPU
 
 ;
 ; Boot sector magic(padding + identification)
