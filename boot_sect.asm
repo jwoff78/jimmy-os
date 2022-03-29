3118f4e584bcd0b738984b78a48fe5f49d5f511a
Khhs167 <khhs1671@gmail.com>
2022-03-29 19:45:21 +0200

    If statements are pog

--- boot_sect.asm
+++ boot_sect.asm
@@ -4,28 +4,31 @@
 
 ;[org 0x7c00] ; Affset all memory readings to where BIOS would start loading the bootloader
 
+mov bx, 30
+
+; if bx <= 4, run(jump to) "if"
+cmp bx, 4
+jle if
+
+; if bx < 40, do "elif"
+cmp bx, 40
+jl elif
+
+; Otherwise, do "else"
+jmp else
+
+if:
+    mov al, 'A'
+    jmp end ; Jump to end of loop
+elif:
+    mov al, 'B'
+    jmp end ; Jump to end of the loop
+else:
+    mov al, 'C'
+end:
+
+
 mov ah, 0x0e ; int 10/ah = 0eh -> print
-
-mov bp, 0x8000 ; Set base of stack above boot sector
-mov sp, bp ; Resets stack
-
-; Push these chars onto stack. They will be convertex to 16-bit by appending 0x00 to the end.
-; The value wont change but the size doubles(0x01 becomes 0x0100 wich both is worth 1)
-
-push 'A'
-push 'B'
-push 'C'
-
-pop bx ; Pop the current 16-bit stack value. Note that this is 16 bits(one word)
-mov al, bl ; Copy the low bits of bl to al(0x12 in the number 0x1234)
-int 0x10 ; Print al
-
-pop bx ; Pop next value
-mov al, bl
-int 0x10 ; Print al
-
-mov al, [0x7ffe] ; To prove the stack grows down, fetch the char at
-                ; 0x8000 - 0x2 (i.e 16 bits)
 int 0x10        ; print(al)
 
 jmp $ ; Just hang the CPU
